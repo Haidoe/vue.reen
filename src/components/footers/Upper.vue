24901308885c41d3bc6587f96050d2926b77c2f8
Haidren Amalia <haidren.amalia@gmail.com>
2019-05-04 02:36:37 +0800

    Optimize Upper Footer, Fluid

--- src/components/footers/Upper.vue
+++ src/components/footers/Upper.vue
@@ -1,6 +1,6 @@
 <template>
     <footer class="upper">
-        <div class="upper-footer-container">
+        <div class="container">
             
             <div>
                 <h3> WHO WE ARE </h3>
@@ -8,7 +8,7 @@
                 <p>Magnis modipsae voloratati andigen daepeditem quiate re porem que aut labor. Laceaque eictemperum quiae sitiorem rest non restibusaes maio es dem tumquam.</p>
                 <b> 
                     More about us 
-                    <span class="footer-icon" id="right-arrow"></span>
+                    <font-awesome-icon icon="arrow-right"></font-awesome-icon>
                 </b>
             </div>
             
@@ -26,17 +26,17 @@
                 <h3> GET IN TOUCH </h3>
                 <p> Doloreiur quia commolu ptatemp dolupta oreprerum tibusam eumenis et consent accullignis dentibea autem inisita. </p>
                 <p>
-                    <span class="footer-icon" id="location"></span> 
+                    <font-awesome-icon icon="map-marker-alt"></font-awesome-icon>
                     Muntinlupa City, Philippines
                 </p>
                 
                 <p>
-                    <span class="footer-icon" id="phone"></span>
+                    <font-awesome-icon icon="mobile-alt"></font-awesome-icon>
                     +00 (123) 45678 90
                 </p>
                 
                 <p>
-                    <span class="footer-icon" id="message"></span>
+                    <font-awesome-icon icon="envelope"></font-awesome-icon>
                     haidren.amalia@gmail.com
                 </p>
             </div>
@@ -57,130 +57,95 @@
     </footer>
 </template>
 
-<style scoped>
-    footer.upper {
-        background: #2F4052;
-        height: 365px;
-    }
+<style lang="sass" scoped>
+    .upper 
+        background: #2F4052
+        height: auto
 
-    .upper-footer-container {
-        width: 70%;
-        box-sizing: border-box;
-        text-align: center;
-        margin: 0 auto;
-        font-family: "Source Sans Pro";
-        display: flex;
-        align-items: flex-start;
-        justify-content: center;
-        max-width: 1366px;
-    }
+        .container
+            display: flex
+            align-items: flex-start
+            justify-content: center
+            padding: 40px 15px
+            
+            @media (max-width: 1024px)
+                flex-wrap: wrap
+                padding: 20px 15px
+            
+            svg
+                margin-right: 10px
+                margin-left: 10px
+            div
+                text-align: left
+                padding: 5px
+                color: #FFF
+                flex-basis: 25%
+                
+                @media (max-width: 1024px)
+                    flex-basis: 45%
+                    margin: 15px 0
+                    padding: 10px
 
-    .upper-footer-container div {
-        box-sizing: border-box;
-        width: calc(100%/4);
-        position: relative;
-        top: 40px;
-        text-align: left;
-        color: #FFF;
-        margin: 0 15px;
-    }
-    .upper-footer-container div h3 {
-        font-family: "Tahoma";
-        font-size: 16pt;
-        margin-top: 0;
-    }
+                @media (max-width: 800px)
+                    flex-basis: 90%
+                    text-align: center
 
-    .upper-footer-container .footer-icon {
-        display: inline-block;
-        background: url("../../assets/images/footer/footer-icon-sprite.png") no-repeat;
-        margin-right: 10px;
-    }
+                h3 
+                    font-family: "Tahoma"
+                    font-size: 16pt
+                    margin-top: 0
+    
+                p
+                    font-size: 14pt
+                    color: #A3B1BF
+                
+                b 
+                    font-size: 15pt
+                    color: A3B1BF
 
-    .upper-footer-container #right-arrow {
-        background-position: 0 -64px;
-        height: 9px;
-        width: 10px;
-    }
+                figure 
+                    margin: 0
+                    padding: 0
 
-    .upper-footer-container #location {
-        background-position: 0 3px;
-        height: 15px;
-        width: 11px;
-    }
+                    img
+                        height: auto
+                        width: 147px
+                        cursor: pointer
+                        margin-left: 3px
+    
 
-    .upper-footer-container #message {
-        background-position: 0 -43px;
-        height: 12px;
-        width: 11px;
-    }
+                form 
+                    display: flex
 
-    .upper-footer-container #phone {
-        background-position: 0 -17px;
-        height: 17px;
-        width: 11px;
-    }
+                    .subscribe 
+                        background-color: #4E5C6C
+                        border: none
+                        height: 40px
+                        border-top-left-radius: 5px
+                        border-bottom-left-radius: 5px
+                        flex-grow:2
+                        padding-left: 15px
+                        color: #A3B1BF
+                        font-size: 13pt
+    
 
-    .upper-footer-container div p {
-        font-size: 14pt;
-        color: #A3B1BF;
-    }
+                        &::placeholder 
+                            color: #A3B1BF
+                            font-family: "Source Sans Pro"
+                            padding-top: 5px
+                            font-size: 13pt
 
-    .upper-footer-container div b {
-        font-size: 15pt;
-        color: #A3B1BF;
-    }
-
-    .upper-footer-container div figure {
-        margin: 0;
-        padding: 0;
-    }
-
-    .upper-footer-container div figure img {
-        height: auto;
-        width: 147px;
-        cursor: pointer;
-    }
-
-    .upper-footer-container form {
-        display: flex;
-    }
-
-    .upper-footer-container form .subscribe {
-        background-color: #4E5C6C;
-        border: none;
-        height: 40px;
-        border-radius: 5px;
-        flex-grow:2;
-        padding-left: 15px;
-        color: #A3B1BF;
-        font-size: 13pt;
-        font-family: "Source Sans Pro";
-    }
-
-    .upper-footer-container form .subscribe::placeholder {
-        color: #A3B1BF;
-        font-family: "Source Sans Pro";
-        padding-top: 5px;
-        font-size: 13pt;
-
-    }
-
-    .upper-footer-container form button {
-        color: #FFF;
-        background-color: #00BC9C;
-        border: none;
-        border-radius: 5px;
-        position: absolute;
-        right: 10px;
-        height: 30px;
-        margin-top: 5px;
-        font-family: "Source Sans Pro";
-        font-weight: bold;
-        cursor: pointer;
-        padding: 2px 10px;
-    }
-
-    .upper-footer-container form button:hover {
-        background: #17A78B;
-    }
+                    button 
+                        color: #FFF
+                        background-color: #00BC9C
+                        border: none
+                        border-top-right-radius: 5px
+                        border-bottom-right-radius: 5px
+                        height: 42px
+                        font-weight: bold
+                        cursor: pointer
+                        padding: 2px 10px
+                        &:hover
+                            :background: #17A78B
+    
 </style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faMapMarkerAlt, faMobileAlt, faEnvelope, faArrowRight } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(faBars)
+library.add(faBars, faMapMarkerAlt, faMobileAlt, faEnvelope, faArrowRight)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
