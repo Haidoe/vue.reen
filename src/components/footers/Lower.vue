c03f7de75369f5a793b3e88b193d4b16b53a012d
Haidren Amalia <haidren.amalia@gmail.com>
2019-05-04 01:28:07 +0800

    Optimized lower footer, Fluid

--- src/components/footers/Lower.vue
+++ src/components/footers/Lower.vue
@@ -1,86 +1,70 @@
 <template>
     <footer class="lower">
-        <div class="lower-footer-container">
-            <div class="left">
-                © 2014 REEN. All rights reserved.
-            </div>
-            <div class="right">
-                <ul>
-                    <li> Home </li>
-                    <li> Portfolio </li>
-                    <li> Blog </li>
-                    <li> About </li>
-                    <li> Services </li>
-                    <li> Contact </li>
-                </ul>
-            </div>
-            <div class="btn-up"></div>
+        <div class="container">
+
+            <p> © 2014 REEN. All rights reserved. </p>
+            
+            <ul>
+                <li> Home </li>
+                <li> Portfolio </li>
+                <li> Blog </li>
+                <li> About </li>
+                <li> Services </li>
+                <li> Contact </li>
+            </ul>
+            
         </div>
+    
     </footer>
 </template>
 
-<style scoped>
-    footer.lower {
-        background: #283645;
-        height: 80px;
-        color: #707E8C;
-        font-family: "Source Sans Pro"
-    }
+<style lang="sass" scoped>
+    .lower 
+        background: #283645
+        height: 80px
+        color: #707E8C
+        
+        @media (max-width:1024px)
+            height: auto
+            padding: 10px
+    
+        .container
+            display: flex
+            justify-content: space-between
+            align-items: center
+            padding: 0 15px
+            
+            @media (max-width: 700px)
+                justify-content: center
 
-    .lower-footer-container {
-        width: 70%;
-        height: inherit;
-        margin: 0 auto;
-        padding: 0 15px;
-        box-sizing: border-box;
-        max-width: 1366px;
-    }
-    .lower-footer-container div.left {
-        margin: 30px 0;
-    }
+            p
+                @media (max-width: 1150px)
+                    font-size: .9em
+                    
+            ul
+                @media (max-width: 700px)
+                    display: none
+            li 
+                display: inline
+                font-weight: bold
+                margin: 0 10px
+                cursor: pointer
+                &:hover
+                    color: #00BC9C
 
-    .lower-footer-container div.right ul {
-        margin: 30px 0;
-    }
+                @media (max-width: 1150px)
+                    font-size: .9em
+                    margin: 0 5px
 
-    .lower-footer-container div.right ul li {
-        display: inline;
-        font-weight: bold;
-        margin: 0 10px;
-    }
+            li + li::before 
+                content: "·"
+                padding-right: 15px
+                color: #707E8C
 
-    .lower-footer-container div.right ul li:hover {
-        cursor: pointer;
-        color: #00BC9C;
-    }
+                @media (max-width: 1150px)
+                    padding-right: 15px
 
-    .lower-footer-container div.right ul li + li::before {
-        content: "·";
-        padding-right: 15px;
-        color: #707E8C;
-    }
-
-    .lower-footer-container div.right ul li + li:hover::before {
-        text-decoration: none !important;
-        cursor: default;
-    }
-
-    footer.lower .left {
-        float: left
-    }
-
-    footer.lower .right {
-        float: right;
-    }
-
-    footer.lower .btn-up {
-        position: absolute;
-        margin-top: 20px;
-        right: 30px;
-        height: 40px;
-        width: 40px;
-        background: url("../../assets/images/footer/btn-up.png");
-        cursor: pointer;
-    }
+            li + li:hover::before 
+                cursor: default
 </style>
 
