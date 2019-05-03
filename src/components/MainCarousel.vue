d340b7d4b57f7f1b2228418815c5d975e0c48aff
Haidren Amalia <haidren.amalia@gmail.com>
2019-05-03 17:10:49 +0800

    Optimized Carousel Section, Fluid

--- src/components/MainCarousel.vue
+++ src/components/MainCarousel.vue
@@ -1,79 +1,116 @@
 <template>
-    <section class="slider">
-			<div class="slider-container">
-				<div class="slider-content">
-					<p class="header"> Hi, I'm a developer </p>
-					<p>
-						Created this online portfolio with the professionally and lovingly designed REEN website template. 
-                    </p>
-					<button>GET STARTED NOW</button>
-				</div>
-			</div>
-		</section>
+    <section class="slider"
+        :style="{ backgroundImage: 'url(' + require(`@/assets/images/carousel/${imgUrl}`) + ')' }">
+
+        <div class="container">
+            <div class="content">
+                <h1> Hi, I'm a developer</h1>
+
+                <p>
+                    Created this online portfolio with the professionally and lovingly designed REEN website template. 
+                </p>
+                <button>GET STARTED NOW</button>
+            </div>
+        </div>
+
+    </section>
 </template>
 
-<style scoped>
-    section.slider {
-        height: 880px;
-        width: 100%;
-        margin: 0;
-        padding: 0;
-        background: url("../assets/images/carousel/carousel-1.png") no-repeat;
-        background-size: cover;
-    }
+<style lang="sass" scoped>
+    section.slider
+        box-sizing: border-box
+        height: 820px
+        width: 100%
+        margin: 0 auto
+        padding: 0
+        background-size: cover
+        background-repeat: no-repeat
+        background-attachment: fixed
+        background-position: center center
+        max-width: 1920px
+        
+        @media only screen and (max-width: 1310px)
+            height: 600px
 
-    .slider .slider-container {
-        width: 70%;
-        height: inherit;
-        padding: 0;
-        margin: 0 auto;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        max-width: 1366px;
-    }
+        @media only screen and (max-width: 800px)
+            height: 400px
 
-    .slider .slider-container .slider-content {
-        text-align: center;
-        width: 60%;
-        margin: auto;	
-    }
+        .container
+            display: flex
+            justify-content: center
+            align-items: center
+            text-align: center
+        
+            .content
+                
+                h1
+                    font-size: 50pt
+                    font-family: 'tahoma'
+                    color: #FFF
+                    text-transform: uppercase
+                    margin: 0
+                    font-weight: bold
+                    width: 90%
+                    margin: 0 auto
+                    
+                    @media only screen and (max-width: 1310px)
+                        font-size: 3em
 
-    .slider .slider-container .slider-content p.header {
-        font-size: 50pt;
-        font-family: 'tahoma';
-        color: #FFF;
-        text-transform: uppercase;
-        margin: 0;
-        font-weight: bold;
-    }
+                    @media only screen and (max-width: 800px)
+                        font-size: 2.5em
+                p 
+                    font-size: 18pt
+                    font-family: "Source Sans Pro"
+                    color:#A3B1BF
+                    margin: 5px 0
+                    width: 80%
+                    margin: 0 auto
+                    margin-top: 20px
 
-    .slider .slider-container .slider-content p {
-        font-size: 18pt;
-        font-family: "Source Sans Pro";
-        color:#A3B1BF;
-        margin: 5px 0;
-    }
+                    @media only screen and (max-width: 1310px)
+                        font-size: 16pt
+                    
+                    @media only screen and (max-width: 800px)
+                        font-size: 14pt
 
-    .slider .slider-container .slider-content button {
-        color: #FFF;
-        height: 50px;
-        width: 250px;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        background: #00BC9C;
-        border: none;
-        margin: 10px auto;
-        border-radius: 5px;
-        font-size: 10pt;
-        font-family: "tahoma";
-        font-weight: bold;
-        margin-top: 30px;
-    }
+                button 
+                    color: #FFF
+                    height: 50px
+                    width: 250px
+                    display: flex
+                    justify-content: center
+                    align-items: center
+                    background: #00BC9C
+                    border: none
+                    margin: 10px auto
+                    border-radius: 5px
+                    font-size: 10pt
+                    font-family: "tahoma"
+                    font-weight: bold
+                    margin-top: 30px
+                    cursor: pointer
 
-    .slider .slider-container .slider-content button:hover {
-        cursor: pointer;
-        background: #17A78B;
-    }
+                    @media only screen and (max-width: 1310px)
+                        height: 40px
+                        width: 200px
+                        font-size: 8pt
+                    
+                    @media only screen and (max-width: 800px)
+                        height: 40px
+                        width: 150px
+                        font-size: 8pt
+
+                    &:hover
+                        background: #17A78B
 </style>
+
+<script>
+    export default {
+        data: function() {
+            return {
+                imgUrl: 'carousel-1.png'
+            }
+        }
+    }
+</script>
+
